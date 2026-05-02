36b44fa8c01fd048cff30a9769340e2648de1663
aubrey <mingsongli@nuaa.edu.cn>
2026-05-02 13:32:33 +0800

    boards: remove unused CANFCU Mini UART7 flow control pins

--- boards/pogo/canfcu-mini/nuttx-config/include/board.h
+++ boards/pogo/canfcu-mini/nuttx-config/include/board.h
@@ -392,8 +392,7 @@
 
 #define GPIO_UART7_RX    GPIO_UART7_RX_3    /* PE7  */
 #define GPIO_UART7_TX    GPIO_UART7_TX_3    /* PE8  */
-#define GPIO_UART7_RTS   GPIO_UART7_RTS_1   /* PE9  */
-#define GPIO_UART7_CTS   (GPIO_UART7_CTS_1 | GPIO_PULLDOWN) /* PE10 */
+
 
 #define GPIO_UART8_RX    GPIO_UART8_RX_1    /* PE0 */
 #define GPIO_UART8_TX    GPIO_UART8_TX_1    /* PE1 */
